94446ecf677ed68228a26bc650104683e9ba3685
idanceinbetween <46828701+idanceinbetween@users.noreply.github.com>
2019-05-14 23:15:45 +0100

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,24 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
   }
+
+  handleTyping = (message) => {
+    this.setState({message: message})
+  }
+
+  handleCalculation = () => this.props.maxChars - this.state.message.length
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} placeholder="Write your message here" onChange={event => this.handleTyping(event.target.value)}/>
+        <i>{this.handleCalculation()} characters remaining</i>
       </div>
     );
   }
